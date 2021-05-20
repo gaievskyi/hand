2271e90c9f14f77c278a6ad3a0ec08e965f87b20
01101101011000010110011100110111 <i.maximys02@gmail.com>
2021-05-20 16:25:39 +0200

    incline behaviour refactored

--- hand.ino
+++ hand.ino
@@ -2,65 +2,67 @@
 // #include <ServoEasing.h> // TODO set up the easing lib
 #include <Stepper.h>
 
-#define pinX  A2
-#define pinY  A3
+#define pinX A2
+#define pinY A3
 
 const int stepsPerRevolution = 200;
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
-// MPU6050 accelerometer; // TODO accelerometer
-
-int servoPin = 3;
-int counterX = 0;
-Servo servo;
-// ServoEasing base_rotation, base_lift, middle_lift, grip;  //  TODO Create 4 servos for each joint
+int servoPin1 = 1;
+int servoPin2 = 2;
+int servoPin3 = 3;
+float incline = 0;
+Servo servo1;
+Servo servo2;
+Servo servo3;
 
 float movementX;
-bool isConnected;  // TODO accelerometer connection
+
+void changeIncline(Servo engine) {
+  float incline = engine.read();
+  
+  if (digitalRead(7) == 0 && incline < 180) {
+    engine.write(++incline);
+  } else if (digitalRead(6) == 0 && incline > 0) {
+    engine.write(--incline);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Initialize sketch");
 
-//  accelerometer.initialize(); // Initialize accelerometer
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  servo3.attach(servoPin3);
 
-  servo.attach(servoPin);
-
-  // TODO Set up the base rotation
-
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
   myStepper.setSpeed(60);
-
-  // do {
-  //   isConnected = accelerometer.testConnection();  //  Test connection with accelerometer
-  //   Serial.println("[!] Not connected.");
-
-  // } while (!isConnected);
-
-  // Serial.println("[*] Connected successfully.");
 }
 
 void loop() {
-  float X = analogRead(pinX);
-  float Y = analogRead(pinY);
-  Serial.print(Y); // inspect the `Y` value
+  delay(20);
 
+  float X = analogRead(pinX); // joystick axis X
+  float Y = analogRead(pinY); // joystick axis Y
+
+  if (digitalRead(12) == 0) { // btn pressed
+    changeIncline(servo1);
+  } else {
+    changeIncline(servo2);
+  }
+
+  if (X > 650 && incline < 180) {
+    servo3.write(++incline);
+  } else if (X < 350 && incline > 0) {
+    servo3.write(--incline);
+  }
+  
   if (Y > 650) {
     myStepper.step(32);
-  }
-  else if (Y < 350) {
+  } else if (Y < 350) {
     myStepper.step(-32);
   }
-  if (X > 650 && movementX < 180  ) {
-    servo.write(++counterX);
-  }
-  else if (X < 350 && movementX > -180 ) {
-    servo.write(--counterX);
-  }
-
-  movementX = (float) (X/1024*180);
-
-  
-
-  // TODO remove tests for deploy
-  //  Serial.println(accelerometer.testConnection());
-}
+}x
